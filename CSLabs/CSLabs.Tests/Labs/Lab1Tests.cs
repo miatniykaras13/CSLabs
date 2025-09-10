3796d25b12ee366fd6e1b978be363dc17ba1f396
Vlad PC <vladosggg007@gmail.com>
2025-09-10 11:14:49 +0300

    o

--- CSLabs/CSLabs.Tests/Labs/Lab1Tests.cs
+++ CSLabs/CSLabs.Tests/Labs/Lab1Tests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSLabs.Labs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace CSLabs.Tests.Labs
 {
-    internal class Lab1Tests
+    public class Lab1Tests
     {
+        private readonly Lab1 _lab;
+        private readonly string _sequencesText = "6.8 kDa mitochondrial proteolipid\tHomo sapiens (Human)\tMLQSIIKNIWIPMKPYYTKVYQEIWIGMGLMGFIVYKIRAADKRSKALKASAPAPGHH\r\nPre-T/NK cell associated protein 6H9A\tHomo sapiens (Human)\tMRLSCLVIITITAELCVPLMLCAHGEQAQLPRGVCVLGTGTSPAWSPVLLGRLPFPH\r\nAlcohol dehydrogenase\tBrachydanio rerio\tMDTTGKVIKCKAAVAWEAGKPLTIEEVEVAPPKAHEVRVKIHATGVCHTDAYTLSGSDPEGLFPVILGHEGAGTVESVGEGVTK\r\nRNA-dependent RNA polymerase [Fragment]\tSan Miguel sea lion virus\tPSGMPLTSIINSLNHCLMVGCAVVKALEDSGVQATWNIFDSMDLFTYGDDGVYIVPPLISSVMPKVFSNLRQFGLKPTRTDKTDAEITPIPADEPVEFLKRTIVRTENGIRALLDKSSII\r\nCecropin\tBombyx mori (Silk moth)\tRWKIFKKIEKVGQNIRDGIVKAGPAVAVVGQAATI";
+        private readonly string _commandsText = "search\tSIIK\r\nsearch\tPLML\r\nsearch\tFK3I\r\ndiff\t6.8 kDa mitochondrial proteolipid\tAlcohol dehydrogenase\r\ndiff\tRNA-dependent RNA polymerase [Fragment]\tAlcohol dehydrogenase\r\ndiff\tCecropin\tPre-T/NK cell associated protein 6H9A\r\nmode\tCecropin\r\nmode\tAlcohol dehydrogenase";
+        public Lab1Tests()
+        {
+            _lab = new Lab1(_sequencesText, _commandsText);
+        }
+
     }
 }
