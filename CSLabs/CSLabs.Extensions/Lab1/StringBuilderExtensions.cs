0223f660fac49a4b816919498e99f13172cb1e8d
Vlad PC <vladosggg007@gmail.com>
2025-09-10 08:50:31 +0300

    p

---
+++ CSLabs/CSLabs.Extensions/Lab1/StringBuilderExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLabs.Extensions.Lab1
+{
+    public static class StringBuilderExtensions
+    {
+        private static void AddLines(this StringBuilder geneData, int number)
+        {
+            geneData.AppendLine("--------------------------------------------------------------------------");
+            geneData.Append(string.Format("{0:d3}", number));
+        }
+
+        //failure
+        public static void AddGeneDataSearch(
+            this StringBuilder geneData,
+            int number,
+            string sequence)
+        {
+            geneData.AddLines(number);
+            geneData.Append($"\tsearch\t{sequence} \r\n");
+            geneData.AppendLine("organism                 protein \r\n");
+            geneData.Append("NOT FOUND");
+        }
+
+        //success
+        public static void AddGeneDataSearch(
+            this StringBuilder geneData,
+            int number,
+            string sequence,
+            string organism,
+            string protein)
+        {
+            geneData.AddLines(number);
+            geneData.Append($"\tsearch\t{sequence} \r\n");
+            geneData.Append("organism\t\t\t\tprotein \r\n");
+            geneData.Append($"{organism}\t\t{protein}\r\n");
+        }
+
+
+        public static void AddGeneDataDiffSuccess(this StringBuilder geneData, int number)
+        {
+
+        }
+    }
+}
---
+++ CSLabs/CSLabs/Labs/Lab1.cs
@@ -0,0 +1,67 @@
+﻿using CSLabs.Extensions.Lab1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLabs.Labs;
+public class Lab1(string SequencesText, string CommandsText)
+{
+    private readonly List<string> _proteinDataList = SequencesText.Split("\r\n").ToList();
+
+    private StringBuilder _geneData = new();
+
+    private int _commandCount = 1;
+
+    public string Func1()
+    {
+        var commandList = CommandsText.Split("\r\n").ToList();
+
+        foreach (string command in commandList)
+        {
+            var commandComponents = command.Split("\t");
+            switch (commandComponents[0])
+            {
+                case "search":
+                    Search(commandComponents[1]);
+                    break;
+                case "diff":
+                    Diff(commandComponents[1], commandComponents[2]);
+                    break;
+                    //case "mode":
+                    //    Mode(commandComponents[1]);
+                    //    break;
+
+            }
+        }
+        return _geneData.ToString();
+    }
+
+    private void Search(string sequence)
+    {
+        foreach (var proteinData in _proteinDataList)
+        {
+            var dataComponents = proteinData.Split("\t");
+            if (dataComponents[2].Contains(sequence))
+            {
+                _geneData.AddGeneDataSearch(_commandCount, sequence, organism: dataComponents[1], protein: dataComponents[0]);
+                _commandCount++;
+                return;
+            }
+        }
+        _geneData.AddGeneDataSearch(_commandCount, sequence);
+
+    }
+
+    private int Diff(string protein1, string protein2)
+    {
+        return 1;
+    }
+
+    //private List<(string, int)> Mode(string protein)
+    //{
+
+    //}
+}
+
